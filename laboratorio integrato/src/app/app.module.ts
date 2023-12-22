5bf47ff8d430f548fc6bb8fc83bbea257ee40225
Clara Cosentino <clara98.cosentino@gmail.com>
2023-12-22 15:37:56 +0100

    Intestazione

--- laboratorio integrato/src/app/app.module.ts
+++ laboratorio integrato/src/app/app.module.ts
@@ -1,34 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { DettaglioPartitaComponent } from './pages/dettaglio-partita/dettaglio-partita.component';
-import { PerTeComponent } from './per-te/per-te.component';
-import { TodayMatchesComponent } from './today-matches/today-matches.component';
-import { FooterComponent } from './footer/footer.component';
-import { CalendarioPartiteComponent } from './pages/calendario-partite/calendario-partite.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { DettaglioPartitaComponent } from "./pages/dettaglio-partita/dettaglio-partita-giocata.component";
+import { PerTeComponent } from "./components/per-te/per-te.component";
+import { TodayMatchesComponent } from "./components/today-matches/today-matches.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CalendarioPartiteComponent } from "./pages/calendario-partite/calendario-partite.component";
+import { HeaderDettaglioPartitaComponent } from "./components/header-dettaglio-partita/header-dettaglio-partita.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HeaderComponent,
-    HomePageComponent,
-    DettaglioPartitaComponent,
-    PerTeComponent,
-    TodayMatchesComponent,
-    FooterComponent,
-    CalendarioPartiteComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HeaderComponent,
+        HomePageComponent,
+        DettaglioPartitaComponent,
+        PerTeComponent,
+        TodayMatchesComponent,
+        FooterComponent,
+        CalendarioPartiteComponent,
+        HeaderDettaglioPartitaComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
