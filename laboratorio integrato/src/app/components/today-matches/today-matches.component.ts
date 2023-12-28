f359f2d01548bbca2bfb261ec58eb9d672438ae6
alessiostrano <alessiostrano.48@gmail.com>
2023-12-28 20:32:00 +0100

    orario bug fixed

--- laboratorio integrato/src/app/components/today-matches/today-matches.component.ts
+++ laboratorio integrato/src/app/components/today-matches/today-matches.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from "@angular/core";
 import { MatchDataService } from "../../match-data.service";
 import { TypeHome } from "src/app/_models/homeApi.type";
 import dayjs from "dayjs";
-
+import localizedFormat from "dayjs/plugin/localizedFormat"; // Importa il plugin per il formato localizzato
+import "dayjs/locale/it"; // Importa la lingua italiana
 @Component({
     selector: "app-today-matches",
     templateUrl: "./today-matches.component.html",
@@ -12,12 +13,15 @@ export class TodayMatchesComponent implements OnInit {
     hour = "";
 
     @Input() data: TypeHome = {} as TypeHome;
-
     ngOnInit() {
+        console.log(this.convertHour)
+    }
 
+    convertHour(time: string) {
+        const dateTimeString = `2023-01-01 ${time}`;
+        const hourDayJs = dayjs(dateTimeString);
+        this.hour = hourDayJs.format("HH:mm");
     }
-    convertHour(orario:string) {
-        const hourDayJs = dayjs(orario);
-        return hourDayJs.format("HH:mm");
-    }
+    
+    
 }
