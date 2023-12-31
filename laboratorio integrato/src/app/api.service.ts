81c557f7bd44bb2dc60830ae5a6203ad5863c6dd
alessiostrano <alessiostrano.48@gmail.com>
2023-12-31 12:46:26 +0100

    Calendario con api dinamica finito

--- laboratorio integrato/src/app/api.service.ts
+++ laboratorio integrato/src/app/api.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable, map } from "rxjs";
 import { TypeHome } from "./_models/homeApi.type";
 import { TeamType } from "./_models/divisionTeamApi.type";
+import { CalendarType } from "./_models/calendarApi.type";
 
 @Injectable({
   providedIn: "root",
@@ -13,6 +14,7 @@ export class ApiService {
   private homeUrl = "games/getHomeUnLogged";
 
   private divisione = "teams/getTeams"
+  private calendar = "games/getCalendar?date="
 
   constructor(private http: HttpClient) {}
   getHomeApi() {
@@ -24,5 +26,16 @@ export class ApiService {
     return this.http.get(`${this.baseURL}${this.divisione}`)
     .pipe(map((response:any) => {
         return response as TeamType}));
+  }
+  requestBody = {
+    // Inserisci qui i dati che vuoi inviare al server, se necessario
+  };
+  getCalendarApi(data: string) {
+    return this.http.post(`${this.baseURL}${this.calendar}${data}`, this.requestBody)
+      .pipe(
+        map((response: any) => {
+          return response as CalendarType;
+        })
+      );
   }
 }
