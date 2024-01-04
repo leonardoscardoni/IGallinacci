b3d346bc625ec4e299700a603d999ee1d733210e
Clara Cosentino <clara98.cosentino@gmail.com>
2024-01-04 14:49:21 +0100

    Aggiunta di posizione in classifica

--- laboratorio integrato/src/app/pages/dettaglio-confronto-team/dettaglio-confronto-team.component.ts
+++ laboratorio integrato/src/app/pages/dettaglio-confronto-team/dettaglio-confronto-team.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-dettaglio-confronto-team',
-  templateUrl: './dettaglio-confronto-team.component.html',
-  styleUrls: ['./dettaglio-confronto-team.component.scss']
+    selector: "app-dettaglio-confronto-team",
+    templateUrl: "./dettaglio-confronto-team.component.html",
+    styleUrls: ["./dettaglio-confronto-team.component.scss"],
 })
 export class DettaglioConfrontoTeamComponent {
-  a = {
-    bgImg: "/assets/confronto-giocatori-header.jpg",
-    nome1: "Clara Cosentino",
-    nome2: "Alessio Strano",
-    logo1: "/assets/s-l1200.webp",
-    logo2: "/assets/s-l1200.webp",
-    
-};
+    a = {
+        bgImg: "/assets/confronto-giocatori-header.jpg",
+        nome1: "Clara Cosentino",
+        nome2: "Alessio Strano",
+        logo1: "/assets/s-l1200.webp",
+        logo2: "/assets/s-l1200.webp",
 
+        classifica: {
+            squadra1: {
+                nome: "Trail brazers",
+                numero: "3",
+            },
+            squadra2: {
+                nome: "Raptors",
+                numero: "34",
+            },
+        },
+    };
 }
