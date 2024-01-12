7055ce09222e226a0206e4dbcb5ff9f757d97f95
alessiostrano <alessiostrano.48@gmail.com>
2024-01-12 10:46:33 +0100

    inizio api filtro giocatori

--- laboratorio integrato/src/app/api.service.ts
+++ laboratorio integrato/src/app/api.service.ts
@@ -6,6 +6,8 @@ import { TeamType } from "./_models/divisionTeamApi.type";
 import { CalendarType } from "./_models/calendarApi.type";
 import { TeamDetailType } from "./_models/TeamDetailApi.type";
 import { ConfrontoTeamType } from "./_models/confrontoTeamApi.type";
+import { PlayerMatchType } from "./_models/playerMatchApi.type";
+import { TeamPlayerFilterType } from "./_models/teamPlayerFilter.type";
 
 @Injectable({
   providedIn: "root",
@@ -19,6 +21,8 @@ export class ApiService {
   private calendar = "game/getCalendar?date="
   private team = "team/getTeamsDetails?idTeam=5&season=2022"
   private confrontoTeam = `${'team/getCompareTeam?idTeam1='}`
+  private playerMatch = "player/getPlayerDetailsByGame?idGame=11058&idPlayer=4"
+  private TeamPlayerFilter = "player/getTeamRolesPlayerFilter"
 
   constructor(private http: HttpClient) {}
   getHomeApi() {
@@ -36,6 +40,16 @@ export class ApiService {
     return this.http.get(`${this.baseURL}${this.team}`)
     .pipe(map((response:any) => {
         return response as TeamDetailType}));
+  }
+  getTeamPlayerFilter() {
+    return this.http.get(`${this.baseURL}${this.TeamPlayerFilter}`)
+    .pipe(map((response:any) => {
+        return response as TeamPlayerFilterType}));
+  }
+  getPlayerMatchApi() {
+    return this.http.get(`${this.baseURL}${this.playerMatch}`)
+    .pipe(map((response:any) => {
+        return response as PlayerMatchType}));
   }
   getConfrontoTeamApi(id1:string,id2:string) {
     return this.http.get(`${this.baseURL}${this.confrontoTeam}${id1}${'&idTeam2='}${id2}${'&season=2022'}`)
--- laboratorio integrato/src/app/pages/scelta-confronto-giocatori/scelta-confronto-giocatori.component.ts
+++ laboratorio integrato/src/app/pages/scelta-confronto-giocatori/scelta-confronto-giocatori.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TeamPlayerFilterType } from 'src/app/_models/teamPlayerFilter.type';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-scelta-confronto-giocatori',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./scelta-confronto-giocatori.component.scss']
 })
 export class SceltaConfrontoGiocatoriComponent {
-
+  
+  constructor(private apiService: ApiService) {}
+  data: TeamPlayerFilterType = {} as TeamPlayerFilterType
+  ngOnInit() {
+      this.apiService.getTeamPlayerFilter().subscribe((data) => {
+        this.data = data;
+        console.log(this.data)
+      });
+    }
 }
