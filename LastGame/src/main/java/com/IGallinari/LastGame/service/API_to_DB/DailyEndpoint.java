241ca5a21145d1af075e6f0b9f9b9fb6c6fe4daa
leonardoscardoni <leonardoscardoni@edu.itspiemonte.it>
2024-01-13 15:58:11 +0100

    Update DailyEndpoint.java

--- LastGame/src/main/java/com/IGallinari/LastGame/service/API_to_DB/DailyEndpoint.java
+++ LastGame/src/main/java/com/IGallinari/LastGame/service/API_to_DB/DailyEndpoint.java
@@ -11,8 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
-//@Component
+@Component
 @AllArgsConstructor
 public class DailyEndpoint {
 
@@ -24,7 +23,7 @@ public class DailyEndpoint {
 
     private StatsGameRepository statsGameRepository;
 
-    //@PostConstruct
+    @PostConstruct
     public void init() throws InterruptedException {
         LocalDate today=LocalDate.now();
         System.out.println("Starting daily call");
