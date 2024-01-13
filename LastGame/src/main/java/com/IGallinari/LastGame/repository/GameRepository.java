22eb5962c13366e2bd13239ed6a91fddf1bad69c
leonardoscardoni <leonardoscardoni@edu.itspiemonte.it>
2024-01-13 15:42:17 +0100

    Update GameRepository.java

--- LastGame/src/main/java/com/IGallinari/LastGame/repository/GameRepository.java
+++ LastGame/src/main/java/com/IGallinari/LastGame/repository/GameRepository.java
@@ -26,7 +26,7 @@ public interface GameRepository extends JpaRepository<Game,Integer> {
     @Query(value = "SELECT g.id FROM Game AS g WHERE g.date <:inputDate ORDER BY g.date DESC; ", nativeQuery = true)
     List<Integer> findAllGameIdsBeforeDate(@Param("inputDate") LocalDate inputDate);
 
-    @Query(value = "SELECT id FROM game g WHERE (g.id NOT IN (SELECT sp.idGame FROM statsplayer sp) OR g.id NOT IN (SELECT sg.idGame FROM statsgame sg)) and g.date<:inputDate ORDER BY g.date DESC; ", nativeQuery = true)
+    @Query(value = "SELECT g.id FROM game g WHERE (g.id NOT IN(SELECT sp.idGame FROM statsplayer sp GROUP BY sp.idGame) OR g.id NOT IN(SELECT sg.idGame FROM statsgame sg GROUP BY sg.idGame)) AND g.status=3 AND g.season=:inputDate ", nativeQuery = true)
     List<Integer> findAllGameIdsBeforeDateNotCompleted(@Param("inputDate") LocalDate inputDate);
 
     List<Game> findGameByDate(LocalDate inputDate);
