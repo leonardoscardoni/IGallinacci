53decd28c35c0d423e37ebced9fb6e4f37a01cca
leonardoscardoni <leonardoscardoni@edu.itspiemonte.it>
2024-01-17 12:11:56 +0100

    Update LastGameApplication.java

--- LastGame/src/main/java/com/IGallinari/LastGame/LastGameApplication.java
+++ LastGame/src/main/java/com/IGallinari/LastGame/LastGameApplication.java
@@ -1,6 +1,5 @@
 package com.IGallinari.LastGame;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class LastGameApplication {
 
-	public static void main(String[] args) throws JsonProcessingException {
+	public static void main(String[] args) {
 		SpringApplication.run(LastGameApplication.class, args);
 	}
 
