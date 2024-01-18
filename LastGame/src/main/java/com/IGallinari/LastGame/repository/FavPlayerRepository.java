f33bc7e4f675260b9884fe7178253f91ed87c274
leonardoscardoni <leonardoscardoni@edu.itspiemonte.it>
2024-01-18 07:55:15 +0100

    Update FavPlayerRepository.java

--- LastGame/src/main/java/com/IGallinari/LastGame/repository/FavPlayerRepository.java
+++ LastGame/src/main/java/com/IGallinari/LastGame/repository/FavPlayerRepository.java
@@ -10,4 +10,7 @@ import java.util.List;
 public interface FavPlayerRepository  extends JpaRepository<FavPlayer,Integer> {
     @Query(value = "SELECT fp.idPlayer FROM favplayer fp WHERE fp.idUser=:inputIdUser", nativeQuery = true)
     List<Integer> findFavPlayersByUser(@Param("inputIdUser") int idUser);
+
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM favplayer WHERE idUser = :inputIdUser AND idPlayer = :inputIdPlayer)", nativeQuery = true)
+    boolean existsByIdUserAndIdTeam(@Param("inputIdUser") int idUser, @Param("inputIdPlayer") int idPlayer);
 }
