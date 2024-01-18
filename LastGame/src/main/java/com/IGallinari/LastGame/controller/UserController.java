e99ee16324021ed51f5d008cda428894517beda3
Lucas-Giavelli <127490632+Lucas-Giavelli@users.noreply.github.com>
2024-01-18 14:38:07 +0100

    Update UserController.java

--- LastGame/src/main/java/com/IGallinari/LastGame/controller/UserController.java
+++ LastGame/src/main/java/com/IGallinari/LastGame/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.IGallinari.LastGame.controller;
 
+import com.IGallinari.LastGame.payload.request.TokenRequest;
 import com.IGallinari.LastGame.payload.request.user.LoginRequest;
 import com.IGallinari.LastGame.payload.request.user.SigninRequest;
 import com.IGallinari.LastGame.payload.response.user.LoginResponse;
 import com.IGallinari.LastGame.payload.response.user.SigninResponse;
 import com.IGallinari.LastGame.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,4 +23,7 @@ public class UserController {
 
     @PostMapping("/signin")
     public SigninResponse signin(@RequestBody SigninRequest signinRequest){return this.service.signin(signinRequest);}
+
+    @RequestMapping("getUserProfile")
+    public ResponseEntity<?> getUserProfile(@RequestBody TokenRequest tokenRequest){return this.service.buildProfile(tokenRequest);}
 }
